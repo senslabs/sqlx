1f10f2e9d151ce11f143eabfb04b993f92b6bff7
Mayank <mayank@dozee.io>
2020-04-04 20:05:50 +0530

    Updating paths from jmoiron to senslabs

--- named.go
+++ named.go
@@ -21,7 +21,7 @@ import (
 	"strconv"
 	"unicode"
 
-	"github.com/jmoiron/sqlx/reflectx"
+	"github.com/senslabs/sqlx/reflectx"
 )
 
 // NamedStmt is a prepared statement that executes named queries.  Prepare it
